17ea39405f9fb7ae1368ea87ea68f13eb4f0babd
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2022-05-04 10:56:35 +0700

    Refactoring.

--- Assets/Scripts/Common/CanvasInputSystem.cs
+++ Assets/Scripts/Common/CanvasInputSystem.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 namespace Common
 {
-    public class CanvasInputSystem: MonoBehaviour, IInputSystem
+    public class CanvasInputSystem : MonoBehaviour, IInputSystem
     {
         [SerializeField] private Camera _camera;
         [SerializeField] private EventTrigger _eventTrigger;
@@ -18,13 +18,13 @@ namespace Common
         {
             var pointerDown = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
             pointerDown.callback.AddListener(data => { OnPointerDown((PointerEventData)data); });
-    
+
             var drag = new EventTrigger.Entry { eventID = EventTriggerType.Drag };
             drag.callback.AddListener(data => { OnPointerDrag((PointerEventData)data); });
-    
+
             var pointerUp = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
             pointerUp.callback.AddListener(data => { OnPointerUp((PointerEventData)data); });
-    
+
             _eventTrigger.triggers.Add(pointerDown);
             _eventTrigger.triggers.Add(drag);
             _eventTrigger.triggers.Add(pointerUp);
@@ -34,17 +34,17 @@ namespace Common
         {
             MouseDown?.Invoke(this, GetWorldPosition(e.position));
         }
-    
+
         private void OnPointerDrag(PointerEventData e)
         {
             MouseDrag?.Invoke(this, GetWorldPosition(e.position));
         }
-    
+
         private void OnPointerUp(PointerEventData e)
         {
             MouseUp?.Invoke(this, GetWorldPosition(e.position));
         }
-    
+
         private Vector2 GetWorldPosition(Vector2 screenPosition)
         {
             return _camera.ScreenToWorldPoint(screenPosition);
